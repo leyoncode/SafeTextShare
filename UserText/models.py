83f11da270c1f56d1711252a06048dc39d5e6950
Leyon <leyonik995@gmail.com>
2024-08-29 21:08:04 -0400

    Model to store users' texts

--- UserText/models.py
+++ UserText/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class UserText(models.Model):
+    text = models.TextField()
+    expiry_date = models.DateTimeField(default=timezone.now() + timedelta(days=3)) #set expiry date 3 days after  posting
+    ip_address = models.GenericIPAddressField()
+
+    def __str__(self):
+        return f"{self.text[:20]}... text from {self.ip_address} expires on {self.expiry_date}"
